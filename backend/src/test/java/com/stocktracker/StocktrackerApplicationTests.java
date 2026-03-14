59949396157b323704c49c1cccfba187e477418f
AngKoonLiang <koonliang.ang@temus.com>
2026-03-14 18:00:28 +0800

    fix test error

--- backend/src/test/java/com/stocktracker/StocktrackerApplicationTests.java
+++ backend/src/test/java/com/stocktracker/StocktrackerApplicationTests.java
@@ -2,9 +2,14 @@ package com.stocktracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
 @SpringBootTest
 class StocktrackerApplicationTests {
+
+    @MockBean
+    ClientRegistrationRepository clientRegistrationRepository;
 
     @Test
     void contextLoads() {
